4cfa501bc59a92a33c6fa0e14071b703c929cd7b
Julio Cruz <atlascruz009@gmail.com>
2016-11-10 16:50:12 -0800

    Added socket.io

---
+++ public/app.js
@@ -0,0 +1,81 @@
+
+var main = function() {
+	'use strict';
+
+		var socket = io.connect('http://localhost:3000', {reconnect: true});
+
+	$('#addQuestionToDatabase').on('click', function (event) {
+
+		if($('#questionInput').val() !== '') {
+
+			var question = $('#questionInput').val();
+			var answer = $('#answerInput').val();
+
+			$.ajax({
+			  type: "POST",
+			  url: 'question',
+			  data: JSON.stringify({
+			  	"Question": question,
+			  	"Answer": answer
+			  }),
+			  success: function(res){
+				$('#answer .result').text('Question: ' + res.Question + " Answer: " + res.Answer);
+				},
+				contentType: "application/json",
+    			dataType: 'json'
+			});
+		}
+		return false;
+	});
+
+	$('#getQuestionFromDatabase').on('click', function (event) {
+
+		$.get('question', function(res){
+			if(res.length === 0){
+				$('#answer_Two .result_Two').text('Database is Empty!');
+			}
+			else{
+				$('#answer_Two .result_Two').text('Question: ' + res.Question + " ID: " + res._id);
+			}
+		});
+		return false;
+	});
+
+	$('#checkAnswerToQuestionInDatabase').on('click', function (event) {
+
+		if($('#idInput').val() !== '') {
+			var ID = $('#idInput').val();
+			var answer = $('#answerInputTwo').val();
+
+			$.ajax({
+			  type: "POST",
+			  url: 'answer',
+			  data: JSON.stringify({
+			  	"ID": ID,
+			  	"Answer": answer
+			  	}),
+			  success: function(res){
+				$('#answer_Three .result_Three').text('Correct: ' + res.correct);
+				},
+			  contentType: "application/json",
+    		  dataType: 'json'
+			});
+		}
+		return false;
+	});
+
+	$('#getScoreFromDatabase').on('click', function (event) {
+
+		$.get('score', function(res){
+			if(res.length === 0){
+				$('#answer_Four .result_Two').text('Database is Empty!');
+			}
+			else{
+				$('#answer_Four .result_Four').text('Right: ' + res.right + " Wrong: " + res.wrong);
+			}
+		});
+		return false;
+	});
+};
+
+$(document).ready(main);
---
+++ server.js
@@ -0,0 +1,135 @@
+var express = require('express'),
+    http = require('http'),
+    bodyParser = require('body-parser'),
+    mongodb = require('mongodb'),
+    mongoose = require('mongoose'),
+    app = express(),
+    MongoClient = mongodb.MongoClient,
+    redis = require('redis'),
+    io = require('socket.io');
+
+client = redis.createClient();
+
+client.on('connect', function() {
+    console.log('connected');
+});
+
+client.set('question_ID', 0);
+client.set('right', 0);
+client.set('wrong', 0);
+
+app.use(express.static(__dirname + '/public'));
+
+// Create server & socket
+server = http.createServer(app).listen(3000);
+server.listen(3000);
+io = io.listen(server);
+console.log('Running on port http://localhost:3000/');
+
+mongoose.Promise = global.Promise;
+// Support JSON-encoded bodies
+app.use(bodyParser.json());
+// Support URL-encoded bodies
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+
+//Connect to the database
+mongoose.Promise = global.Promise;
+mongoose.connect('mongodb://localhost:27017/test');
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {
+    console.log("Connected to Database");
+});
+
+// Home Page
+app.get('/', function(req, res) {
+    //res.render('index');
+    res.sendFile(__dirname + '/index.html');
+});
+
+// Posting new question
+app.post('/question', function(req, res) {
+
+    client.incr('question_ID', function(err, ID) {
+        var collection = db.collection('questions');
+        var input = {
+            "Question": req.body.Question,
+            "Answer": req.body.Answer,
+            "_id": ID
+        };
+
+        collection.insert([input], function(err, result) {
+            if(err){
+                console.log(err);
+            }
+            else{
+                res.json({'Question': input.Question, 'Answer': input.Answer});
+            }
+        });
+    });
+});
+
+
+app.get('/question', function(req, res) {
+    var collection = db.collection('questions');
+    collection.find().toArray(function(err, questions) {
+        res.send(questions[0]);
+    });
+});
+
+
+app.post('/answer', function(req, res) {
+
+    var collection = db.collection('questions');
+    collection.find().toArray(function(err, questions) {
+        var index = 0;
+        var found = 0;
+        while(index < questions.length && found != -1){
+             if(questions[index]._id == req.body.ID && questions[index].Answer == req.body.Answer) {
+                client.incr('right', function(err,result) {});
+                res.json({"correct": "true"});
+                found = -1;
+            }
+            index += 1;
+        }
+
+        if(found === 0)
+        {
+            client.incr('wrong', function(err,result) {});
+            res.json({"correct": "false"});
+        }
+    });
+    // cursor.forEach(function(Question) {
+    //     if(Question._id == req.body.ID && Question.Answer == req.body.Answer) {
+    //         client.incr('right', function(err,result) {});
+    //         res.json({"correct": "true"});
+    //     }
+    //     else{
+    //         client.incr('wrong', function(err,result) {});
+    //         //res.json({"correct": "false"});
+    //     }
+    // });
+});
+
+app.get('/score', function(req, res) {
+    client.get('right', function(err, right) {
+        client.get('wrong', function(err, wrong) {
+            res.json({"right": right,
+                      "wrong": wrong
+                  });
+        });
+    });
+});
+
+// Add a connect listener
+io.on('connection', function(socket) {
+
+    console.log('user connected.');
+
+    // Disconnect listener
+    socket.on('disconnect', function() {
+        console.log('user disconnected.');
+    });
+});
